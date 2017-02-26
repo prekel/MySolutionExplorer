db81f63f12394c8a564400e8cb1b75875bf659d5
Владислав Прекель <79232809414@yandex.ru>
2017-02-26 16:08:10 +0700

    add tests

---
+++ MySolutionExplorer.Tests/UnitTest1.cs
@@ -0,0 +1,22 @@
+﻿using System;
+using Microsoft.VisualStudio.TestTools.UnitTesting;
+using MySolutionExplorer;
+
+namespace MySolutionExplorer.Tests
+{
+	[TestClass]
+	public class UnitTest1
+	{
+		[TestMethod]
+		public void TestMethod1()
+		{
+			Assert.AreEqual(0, 0);
+		}
+
+		[TestMethod]
+		public void TestMethod2()
+		{
+			Assert.AreEqual(0, 1);
+		}
+	}
+}
