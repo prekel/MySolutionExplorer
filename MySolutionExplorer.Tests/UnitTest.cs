563c7950542d3d06db3fe373e4858b82349fcf1d
Владислав Прекель <79232809414@yandex.ru>
2017-02-27 21:24:54 +0700

    rename

---
+++ MySolutionExplorer.Tests/UnitTest.cs
@@ -0,0 +1,35 @@
+﻿using System;
+using Microsoft.VisualStudio.TestTools.UnitTesting;
+using MySolutionExplorer;
+using System.IO;
+
+namespace MySolutionExplorer.Tests
+{
+	[TestClass]
+	public class UnitTest
+	{
+		[TestMethod]
+		public void CreateSolutionTest()
+		{
+			var s = new Solution();
+		}
+
+		//[TestMethod]
+		//public void CreateCppProjectTest()
+		//{
+		//	var dir = Directory.GetCurrentDirectory();
+		//	var s = new Solution();
+		//	var p = new CppProject
+		//	{
+		//		ParentSolution = s,
+		//		TaskName = "Sample",
+		//		Site = "asd",
+		//		Number = 123,
+		//		Lang = "cpp"
+		//	};
+		//	p.Path = dir + MyEnum.Slash + p.Name;
+		//	p.CreateFiles();
+		//	s.Add(p);
+		//}
+	}
+}
