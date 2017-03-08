dec47bdf399bfdacf9581aea596ee239a2af1033
Владислав Прекель <79232809414@yandex.ru>
2017-03-08 13:11:32 +0700

    тест тестов

--- MySolutionExplorer.Tests/UnitTest.cs
+++ MySolutionExplorer.Tests/UnitTest.cs
@@ -9,27 +9,45 @@ namespace MySolutionExplorer.Tests
 	public class UnitTest
 	{
 		[TestMethod]
+		public void TestTest1()
+		{
+			Assert.AreEqual(1, 1);
+		}
+
+		[TestMethod]
+		public void TestTest2()
+		{
+			Assert.AreEqual(1, 1);
+		}
+
+		[TestMethod]
+		public void TestTest3()
+		{
+			Assert.AreEqual(1, 1);
+		}
+		
+		[TestMethod]
 		public void CreateSolutionTest()
 		{
 			var s = new Solution();
 		}
 
-		//[TestMethod]
-		//public void CreateCppProjectTest()
-		//{
-		//	var dir = Directory.GetCurrentDirectory();
-		//	var s = new Solution();
-		//	var p = new CppProject
-		//	{
-		//		ParentSolution = s,
-		//		TaskName = "Sample",
-		//		Site = "asd",
-		//		Number = 123,
-		//		Lang = "cpp"
-		//	};
-		//	p.Path = dir + MyEnum.Slash + p.Name;
-		//	p.CreateFiles();
-		//	s.Add(p);
-		//}
+		[TestMethod]
+		public void CreateCppProjectTest()
+		{
+			var dir = Directory.GetCurrentDirectory();
+			var s = new Solution();
+			var p = new CppProject
+			{
+				ParentSolution = s,
+				TaskName = "Sample",
+				Site = "asd",
+				Number = 123,
+				Lang = "cpp"
+			};
+			p.Path = dir + MyEnum.Slash + p.Name;
+			p.CreateFiles();
+			s.Add(p);
+		}
 	}
 }
