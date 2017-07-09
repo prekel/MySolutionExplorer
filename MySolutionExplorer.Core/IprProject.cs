bd42592a22ae996eb4d43b9aac83da545478bff5
Владислав Прекель <79232809414@yandex.ru>
2017-07-10 02:43:52 +0700

    ед

--- MySolutionExplorer.Core/IprProject.cs
+++ MySolutionExplorer.Core/IprProject.cs
@@ -38,15 +38,9 @@ namespace MySolutionExplorer.Core
             set => XmlProjectFiles[2] = value;
         }
 
-        protected IprProject() : base(3)
-        {
-            Init();
-        }
+        protected IprProject() : base(3) => Init();
 
-        protected IprProject(string path) : base(path, 3)
-        {
-            Init();
-        }
+        protected IprProject(string path) : base(path, 3) => Init();
 
         private void Init()
         {
@@ -87,7 +81,7 @@ namespace MySolutionExplorer.Core
             ReformIml();
         }
 
-        private void Replace(XmlProjectFile file)
+        protected void Replace(XmlProjectFile file)
         {
             string s;
             using (var sr = new StreamReader(file.File.OpenRead()))
