985a1414b7bcda0abe4db6e85e924efb18ae1072
Владислав Прекель <79232809414@yandex.ru>
2017-09-22 14:19:42 +0700

    sort

--- MySolutionExplorer/MainWindow.xaml.cs
+++ MySolutionExplorer/MainWindow.xaml.cs
@@ -227,5 +227,67 @@ namespace MySolutionExplorer
 				}
 			}
 		}
+
+		private GridViewColumnHeader listViewSortCol = null;
+		private SortAdorner listViewSortAdorner = null;
+
+		private void GridViewColumnHeader_Click(object sender, RoutedEventArgs e)
+		{
+			GridViewColumnHeader column = (sender as GridViewColumnHeader);
+			string sortBy = column.Tag.ToString();
+			if (listViewSortCol != null)
+			{
+				AdornerLayer.GetAdornerLayer(listViewSortCol).Remove(listViewSortAdorner);
+				mainTable.Items.SortDescriptions.Clear();
+			}
+
+			ListSortDirection newDir = ListSortDirection.Ascending;
+			if (listViewSortCol == column && listViewSortAdorner.Direction == newDir)
+				newDir = ListSortDirection.Descending;
+
+			listViewSortCol = column;
+			listViewSortAdorner = new SortAdorner(listViewSortCol, newDir);
+			AdornerLayer.GetAdornerLayer(listViewSortCol).Add(listViewSortAdorner);
+			mainTable.Items.SortDescriptions.Add(new SortDescription(sortBy, newDir));
+		}
+
+		public class SortAdorner : Adorner
+		{
+			private static Geometry ascGeometry =
+					Geometry.Parse("M 0 4 L 3.5 0 L 7 4 Z");
+
+			private static Geometry descGeometry =
+					Geometry.Parse("M 0 0 L 3.5 4 L 7 0 Z");
+
+			public ListSortDirection Direction { get; private set; }
+
+			public SortAdorner(UIElement element, ListSortDirection dir) : base(element)
+			{
+				Direction = dir;
+			}
+
+			protected override void OnRender(DrawingContext drawingContext)
+			{
+				base.OnRender(drawingContext);
+
+				if (AdornedElement.RenderSize.Width < 20)
+					return;
+
+				TranslateTransform transform = new TranslateTransform
+						(
+								AdornedElement.RenderSize.Width - 15,
+								(AdornedElement.RenderSize.Height - 5) / 2
+						);
+				drawingContext.PushTransform(transform);
+
+				Geometry geometry = ascGeometry;
+				if (Direction == ListSortDirection.Descending)
+					geometry = descGeometry;
+				drawingContext.DrawGeometry(Brushes.Black, null, geometry);
+
+				drawingContext.Pop();
+			}
+
+		}
 	}
 }
